722d250288a3198105ac52e3f2744e4dc882d4bc
Myneeraj <138659575+Myneeraj@users.noreply.github.com>
2025-12-03 01:17:50 +0530

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/', express.static('public'));
+
+app.get('/healthz', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
